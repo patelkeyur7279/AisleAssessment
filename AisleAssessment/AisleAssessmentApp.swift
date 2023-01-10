e33d27a076a2dfb5f2da02b74617b5075aa5c8d7
Keyur Patel <patelkeyur7279@gmail.com>
2023-01-11 00:26:38 +0530

    Initial Commit

---
+++ AisleAssessment/AisleAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AisleAssessmentApp.swift
+//  AisleAssessment
+//
+//  Created by Keyur Patel on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct AisleAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
